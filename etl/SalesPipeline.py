eac74c4d3372ce6945f0260537db0c15ab2471cf
1G4S <igor.sarnowski.21@gmail.com>
2026-09-12 11:28:33 +0200

    Correct the function

--- etl/SalesPipeline.py
+++ etl/SalesPipeline.py
@@ -10,14 +10,13 @@ class SalesPipeline:
         self.engine = self._create_db_engine()
 
     def _create_db_engine(self):
-        def _create_db_engine(self):
-            """Connecting to database"""
-            params = urllib.parse.quote_plus(
-                f"Driver={{ODBC Driver 18 for SQL Server}};"
-                f"Server=tcp:{os.environ.get('DB_SERVER')},1433;"
-                f"Database={os.environ.get('DB_NAME')};"
-                f"Uid={os.environ.get('DB_USER')};"
-                f"Pwd={os.environ.get('DB_PASS')};"
-                f"Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-            )
-            return create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
+        """Connecting to database"""
+        params = urllib.parse.quote_plus(
+            f"Driver={{ODBC Driver 18 for SQL Server}};"
+            f"Server=tcp:{os.environ.get('DB_SERVER')},1433;"
+            f"Database={os.environ.get('DB_NAME')};"
+            f"Uid={os.environ.get('DB_USER')};"
+            f"Pwd={os.environ.get('DB_PASS')};"
+            f"Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+        )
+        return create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
